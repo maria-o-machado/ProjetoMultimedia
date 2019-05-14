664d9689f13fdb37e2e8e8b078979c13b67520a0
mariamachado6 <molimpiadias@gmail.com>
2019-05-14 01:23:10 +0100

    Login done!!

--- code/login.js
+++ code/login.js
@@ -11,22 +11,27 @@ function main()
 	var sair = document.getElementsByTagName("button")[1];
 	login.addEventListener("click", loginClickHandler, true);  //intersecta evento na capture phase (i.e., na descida) e não na bubbling phase (i.e., subida, por omissão)
 	sair.addEventListener("click", sairClickHandler, true);
-
 }
 
 
 
 function loginClickHandler(ev)
 {
-	/*var user = window.document.getElementById("inputBox");
-	console.log(user)
-	if(user==null){
-		alert("Invalid username");
-		return;
+	var name =  window.document.getElementById("nome").value;
+	var user = localStorage.getItem(name);
+	if (name == ""){
+		alert("Invalid username!");
 	}
-	console.log("SAII")*/
-	//setCookie("username", user, 365);
-    location.href = "../html/MenuPrincipal.html";
+	else{
+		if(user!=null){
+			alert("User already used!");
+			return;
+		}
+		else{
+			localStorage.setItem(name, name);
+			location.href = "../html/MenuPrincipal.html";
+		}
+	}
 }
 
 function setCookie(cname, cvalue, exdays) {
